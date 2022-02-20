5d94ef3211233b9952d5f544f719f05f035a3743
nigelreign <zulunigelb@gmail.com>
2022-02-20 12:32:39 +0200

    get emails from a certain email address

--- script.py
+++ script.py
@@ -14,19 +14,17 @@ imap = imaplib.IMAP4_SSL("imap.gmail.com")
 # authentication
 imap.login(username, password)
 
+# email address to get emails from
+emails_from = "target@gmail.com"
+
 status, messages = imap.select("INBOX")
 
-def clean(text):
-    # clean text for creating a folder
-    return "".join(c if c.isalnum() else "_" for c in text)
-
-
 # number of top emails to fetch
-N = 3
+number = 10
 # total number of emails
 messages = int(messages[0])
 
-for i in range(messages, messages-N, -1):
+for i in range(messages, messages-number, -1):
     # fetch the email message by ID
     res, msg = imap.fetch(str(i), "(RFC822)")
     for response in msg:
@@ -42,57 +40,27 @@ for i in range(messages, messages-N, -1):
             From, encoding = decode_header(msg.get("From"))[0]
             if isinstance(From, bytes):
                 From = From.decode(encoding)
-            print("Subject:", subject)
-            print("From:", From)
-            # if the email message is multipart
-            if msg.is_multipart():
-                print("+++++++++++++++we here")
-                # iterate over email parts
-                for part in msg.walk():
+
+            start = From.find("<") + len("<")
+            end = From.find(">")
+            search_email = From[start:end]
+            print(search_email)
+            if search_email == emails_from:
+                # if the email message is multipart
+                if msg.is_multipart():
+                    pass    
+                else:
                     # extract content type of email
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+                        # print only text email parts
                         print(body)
-                    elif "attachment" in content_disposition:
-                        # download attachment
-                        filename = part.get_filename()
-                        if filename:
-                            folder_name = clean(subject)
-                            if not os.path.isdir(folder_name):
-                                # make a folder for this email (named after the subject)
-                                os.mkdir(folder_name)
-                            filepath = os.path.join(folder_name, filename)
-                            # download attachment and save it
-                            open(filepath, "wb").write(part.get_payload(decode=True))
+
+                print("="*100)
             else:
-                print("++++++++++++++++no we here now")
-                # extract content type of email
-                content_type = msg.get_content_type()
-                # get the email body
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # print only text email parts
-                    print(body)
-            if content_type == "text/html":
-                # if it's HTML, create a new HTML file and open it in browser
-                folder_name = clean(subject)
-                if not os.path.isdir(folder_name):
-                    # make a folder for this email (named after the subject)
-                    os.mkdir(folder_name)
-                filename = "index.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "w").write(body)
-                # open in the default browser
-                webbrowser.open(filepath)
-            print("="*100)
+                pass
 # close the connection and logout
 imap.close()
 imap.logout()
